fff413e0fdf6fae9467a1b831ac9fb87fd8f0b19
Tim <timhogeweg@gmail.com>
2025-10-13 13:12:22 +0200

    Initial setup of Highway DnD Tools plugin

--- main.ts
+++ main.ts
@@ -1,133 +1,88 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+// main.ts
+// v2 - Cleaned template and added settings tab and initial command.
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+// Define the settings that our plugin will store.
+interface HwysDnDToolsSettings {
+	apiToken: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+// Set the default values for the settings.
+const DEFAULT_SETTINGS: HwysDnDToolsSettings = {
+	apiToken: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// This is the main class for our plugin.
+export default class HwysDnDToolsPlugin extends Plugin {
+	settings: HwysDnDToolsSettings;
 
+	// This function runs when the plugin is first loaded.
 	async onload() {
+		// Load any saved settings from memory.
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// This adds a command to the command palette.
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'insert-caravan-status',
+			name: 'Insert Caravan Status',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				// This is what will run when the user triggers the command.
+				// For now, it just creates a simple notice.
+				// Later, this will call our Firebase API.
+				new Notice('Fetching Caravan Status...');
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// This adds a settings tab so the user can configure the plugin.
+		this.addSettingTab(new HwysDnDToolsSettingTab(this.app, this));
 	}
 
+	// This function runs when the plugin is disabled.
 	onunload() {
 
 	}
 
+	// This function loads the plugin's settings.
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
+	// This function saves the plugin's settings.
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+// This class defines the UI for our settings tab.
+class HwysDnDToolsSettingTab extends PluginSettingTab {
+	plugin: HwysDnDToolsPlugin;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HwysDnDToolsPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
+	// This function creates the HTML elements for the settings tab.
 	display(): void {
 		const {containerEl} = this;
 
+		// Clear any old settings from the screen.
 		containerEl.empty();
 
+		// Add a heading for our settings section.
+		containerEl.createEl('h2', {text: 'Highway DnD Tools Settings'});
+
+		// Add the setting for the API Token.
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('API Token')
+			.setDesc('Paste your personal API token generated from the Highway DnD Tools website.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your API token')
+				.setValue(this.plugin.settings.apiToken)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					// When the user types in the box, update the setting and save it.
+					this.plugin.settings.apiToken = value;
 					await this.plugin.saveSettings();
 				}));
 	}
